482f9aa83fa8d081b563a0989989e1982a164c22
reno9 <49720596+reno9@users.noreply.github.com>
2020-01-20 18:01:01 +0800

    Create manage.py

---
+++ 周春鹏+阶段1/manage.py
@@ -0,0 +1,15 @@
+
+from flask_migrate import MigrateCommand
+from flask_script import Manager
+
+from App import create_app
+
+#env = os.environ.get("FLASL_ENV", "develop")
+
+app = create_app('develop')
+
+manager = Manager(app=app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
